c0ca4294e8e3a0ddcd5cb39b547ef6658acae002
Alejandro <cursomeancore@gmail.com>
2020-02-25 18:20:15 +0100

    Subido ejercicio 3.2

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+Ejercicio 2: escribe un programa con cuatro variables de tipo number (a, b, c y d) y un condicional que imprima por pantalla si la suma de a y b es mayor que la suma de c y d.
+*/
+
+const a = 1;
+const b = 6;
+const c = 2;
+const d = 2;
+
+if (a + b > c + d) {
+  console.log('Es mayor');
+  
+}
